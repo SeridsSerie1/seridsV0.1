ed5d503885b7101886505f0efc193ef7c1ead20e
Luis Campos <luiscampostorreta@gmail.com>
2024-05-01 15:42:33 -0400

    docker listo, iniciando la pantalla principal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,10 @@
 import "./bootstrap";
-import { createApp } from "vue";
 
-createApp({}).mount("#app");
+import { createApp } from "vue";
+import TestComponent from "./Components/TestComponent.vue";
+
+const app = createApp({});
+
+app.component("test-component", TestComponent);
+
+app.mount("#app");
