ebfcc8b3a5c7409ebbf71c71c7deb57c5c9d284b
Zoriyu47 <zoriyu47@gmail.com>
2024-05-10 21:08:17 -0400

    zoriyu
    test de dompdf, falta añadirlo como servicio

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use Barryvdh\DomPDF\Facades\Pdf;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/pdftest', function () {
+    return view('test.pdftest');
+});
+
+Route::get('/' , function(){
+    // $pdf = App::make('dompdf.wrapper');
+    //$pdf = app('dompdf.wrapper');
+    $pdf = Pdf:: loadHTML('<h1>hola pdf 3</h1>');
+    //$pdf->loadHTML('<h1>hola pdf 2</h1>');
+    return $pdf->stream();
 });
